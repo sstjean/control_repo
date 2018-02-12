1d4acbee707415f4d867e2320d8f719ad6e77e75
Steven St Jean <steve@sstjean.com>
2018-02-12 08:45:54 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,20 +1,24 @@
 node default {
-
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
-  file { '/root/README':
+  file {'/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn}\n",
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
   }
 }
-node 'minetest.puppet.vm' {
-  include role::minecraft.server
+node 'minetest.puppet.vm': {
+  include role::minecraft_server
 }
-node /^web/ {
+node /^web/ { 
   include role::app_server
 }
-
 node /^db/ {
   include role::db_server
 }
